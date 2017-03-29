624dea251940aded0dd083614f12643c5ac3c98f
Christine Dey <christinedey5@gmail.com>
2017-03-28 20:16:00 -0700

    Base templates for React

---
+++ src/client/app/Chat.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {render} from 'react-dom';
+import $ from 'jquery';
+
+class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+    
+  }
+
+  render() {
+    return (
+    
+    )
+  }
+}
+
+render(<Chat/>, document.getElementById('app'));
