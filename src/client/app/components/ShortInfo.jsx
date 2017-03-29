1d91c5727168547b9fb48fd9aea0bfac908589a8
Li Yi <jayceeyili@gmail.com>
2017-03-29 16:21:53 -0700

    show details if click the shortInfo

--- src/client/app/components/ShortInfo.jsx
+++ src/client/app/components/ShortInfo.jsx
@@ -2,20 +2,29 @@ import React from 'react';
 
 export default class ShortInfo extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+
+    this.state = {
+      showDescription: false
+    };
+
+    this.handleEventInfoClick = this.handleEventInfoClick.bind(this)
   }
-}
 
-// const ShortInfo = props => {
-//   // functions
-//
-//   return (
-//     <div>
-//       <span>{ props.event.title } | </span>
-//       <span>{ props.event.location } | </span>
-//       <span>{ props.event.time } | </span>
-//     </div>
-//   );
-// };
+  handleEventInfoClick() {
+    this.setState({
+      showDescription: !this.state.showDescription
+    })
+  }
 
-export default ShortInfo;
+  render() {
+    return (
+      <div onClick={ this.handleEventInfoClick }>
+        <span>{ this.props.event.title } | </span>
+        <span>{ this.props.event.location } | </span>
+        <span>{ this.props.event.time } | </span>
+        { this.state.showDescription ? <div>{ this.props.event.description }</div> : null }
+      </div>
+    );
+  }
+};
