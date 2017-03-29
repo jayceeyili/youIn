b27d5f2f7c8f615fefb4251784fd02e45c05e0c9
Li Yi <jayceeyili@gmail.com>
2017-03-29 14:55:24 -0700

    move the fake data in order to set the current event

--- server/data.js
+++ server/data.js
@@ -43,7 +43,7 @@ let users = [
     lastname: 'Zirdung',
     photoUrl: 'fredurl',
     email: 'fred@hackreactor.com'
-  } 
+  }
 ];
 let events = [
   {
@@ -147,7 +147,50 @@ let usersEvents = [
   },
 ];
 
+let myEvents = [
+  {
+    event_id: 1,
+    owner: 1,
+    title: 'I Wanna Go To The Beach',
+    'short_desc': 'Beach Party',
+    description: 'Let\'s go to Ocean Beach while the sun is out! BYOB and don\'t joke about sharks, I\'ll kick you out for real.',
+    location: 'Ocean Beach, SF, right off the N Judah.',
+    date: '2017-03-14',
+    time: '05:30:00',
+    attendees: 10,
+    min: 5
+  },
+  {
+    event_id: 2,
+    owner: 2,
+    title: 'Pig, Beer, and Bocce Ball',
+    'short_desc': 'BBQ',
+    description: 'Vegan Only; don\'t be a jerk about it.',
+    location: '456 Fillmore Street, San Francisco, CA 94117',
+    date: '2017-03-14',
+    time: '17:45:00',
+    attendees: 3,
+    min: 15
+  }
+];
+
+let friendEvents = [
+  {
+    event_id: 3,
+    owner: 5,
+    title: 'Moonlight at 8',
+    'short_desc': 'Movie',
+    description: 'Let\'s go see Moonlight',
+    location: 'Downtown AMC Metreon',
+    date: '2017-03-14',
+    time: '20:00:00',
+    attendees: 6,
+    min: 5
+  }
+];
+
+module.exports.myEvents = myEvents;
+module.exports.friendEvents = friendEvents;
 module.exports.users = users;
 module.exports.events = events;
 module.exports.usersEvents = usersEvents;
-
