16558f155f03a7ca455f35d52bb586b2aeecb30c
Terence Tham <terence@epiphanyedu.com>
2017-03-30 14:27:20 -0700

    Passing off IO to winston

--- server/io.js
+++ server/io.js
@@ -1,5 +1,6 @@
 var sockets = require('socket.io');
 var io = null;
+var Message = require('./models/messages');
 
 module.exports = {
 	io: function() {
@@ -8,9 +9,27 @@ module.exports = {
 
 	init: function(server) {
 		io = sockets(server);
-		
+
 		io.on('connection', function(socket) {
-			console.log('connected to sockets: ', socket);
+			console.log('connected to sockets');
+
+			socket.on('send-message', function(data) {
+				console.log('Sockets: received message ', data);
+			  var room = data.event_id
+			  socket.join(`${room}`, function() {
+			    // new message object retrieved from db.
+			    Message.write(data)
+			    	.then(function (result) {
+			    // sends a response of new-message event to all people in the event room.
+				      io.to(`${room}`, 'a new message')
+				      .emit('new-message', result);
+				    })
+				    .catch((err) => {
+				      console.error(err, 'an error in db retrieval');
+				    });
+			  });
+			});
+
 			socket.on('disconnect', function() {
 				console.log('client disconnected');
 			});
