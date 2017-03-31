be9a7dbca0a7b4590f2a057ab81ef6eb6a660780
Terence Tham <terence@epiphanyedu.com>
2017-03-31 13:16:02 -0700

    Update Message model query

--- server/models/messages.js
+++ server/models/messages.js
@@ -10,11 +10,13 @@ module.exports = {
      where user_id = $1 and (current_status=\'accepted\' or\
       current_status=\'pending\')', [userid]);
   },
+
   getUserEvent: function (obj) {
     return db.query('select event_id from users_events\
      where user_id = $1 and event_id = $2 and (current_status=\'accepted\' or\
       current_status=\'pending\')', [obj.user_id, obj.event_id]);
   },
+
   write: function (message) {
     /*
       message objects received in the form of
