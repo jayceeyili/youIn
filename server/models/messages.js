019847bae59250de356a4e6ddb3adf2ac75d58c3
Winston Ku <wdku5791@gmail.com>
2017-03-31 16:08:49 -0700

    adds a path for client to add friends and receive their friendlist

--- server/models/messages.js
+++ server/models/messages.js
@@ -16,6 +16,28 @@ module.exports = {
      where user_id = $1 and event_id = $2 and (current_status=\'accepted\' or\
       current_status=\'pending\')', [obj.user_id, obj.event_id]);
   },
+  
+  getUserFriends: function (userid) {
+    return db.query('select * from users inner join friends\
+     on (users.user_id = friends.user2 and friends.user1 =$1)', [userid]);
+  },
+
+  getUserFriendsID: function (userid) {
+    return db.query('select user_id from users inner join friends\
+     on (users.user_id = friends.user2 and friends.user1 =$1)', [userid]);
+  },
+
+  addUserFriend: function (friendRequest) {
+    
+    /*friend objects received in the form of
+      {
+        user_id: int
+        friend_id: int
+      }*/
+    return db.one('insert into friends (user1, user2)\
+      values (${user_id}, ${friend_id}) returning *', friendRequest);
+
+  },
 
   write: function (message) {
     /*
