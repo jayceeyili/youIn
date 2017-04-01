44493b85c2596e6a2b671b026521b03bdf971c86
Li Yi <jayceeyili@gmail.com>
2017-04-01 16:33:09 -0700

    show user name instead of user id in message

--- src/client/app/components/MessageListEntry.jsx
+++ src/client/app/components/MessageListEntry.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import Moment from 'react-moment';
 
-const MessageListEntry = (props) => (
-  <div className="item">
-    <div className="content">
-      <a className="header">{ props.message.user_name }</a>
-      <div className="meta">
-        <span className="price">{ <Moment fromNow ago>{ props.message.created }</Moment> } ago</span>
-      </div>
-      <div className="description">
-        { props.message.text }
+const MessageListEntry = (props) => {
+  let name = props.message.firstname + ' ' + props.message.lastname;
+  
+  return (
+    <div className="item">
+      <div className="content">
+        <a className="header">{ name }</a>
+        <div className="meta">
+          <span className="price">{ <Moment fromNow ago>{ props.message.created }</Moment> } ago</span>
+        </div>
+        <div className="description">
+          { props.message.text }
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default MessageListEntry;
